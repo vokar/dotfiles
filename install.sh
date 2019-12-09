3384e8b1f8a503c735707f67bb372d1d0d402388
Vo Kar <vokar97@pm.me>
2019-12-10 02:21:29 +0300

    Rename .profile

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -sfrb .profile ~/.profile
+ln -sfrb profile ~/.profile
 ln -sfrb i3config ~/.config/i3/config
 ln -sfrb init.vim ~/.config/nvim/init.vim
 ln -sfrb remaps ~/.scripts/tools/remaps
