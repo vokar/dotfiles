4b553ee22b45b874fcfec3650374cddd27e90c08
Vo Kar <vokar97@pm.me>
2020-01-31 00:39:34 +0300

    Fix install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ ln -sfrb remaps ~/.local/bin/remaps
 ln -sfrb aliasrc ~/.config/aliasrc
 ln -sfrb i3config ~/.config/i3/config
 ln -sfrb init.vim ~/.config/nvim/init.vim
-ln -sfrb inputrc ~/config/.inputrc
+ln -sfrb inputrc ~/.config/.inputrc
 ln -sfrb zshrc ~/.config/zsh/.zshrc
