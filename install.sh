327580eb255a0786fb37082ce68d05c221c11e61
Vo Kar <vokar97@pm.me>
2020-01-30 20:06:03 +0300

    Add aliasrc

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ln -sfrb profile ~/.profile
 ln -sfrb remaps ~/.local/bin/remaps
+ln -sfrb aliasrc ~/.config/aliasrc
 ln -sfrb i3config ~/.config/i3/config
 ln -sfrb init.vim ~/.config/nvim/init.vim
 ln -sfrb inputrc ~/config/.inputrc
