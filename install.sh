f89d3982f32391472f8b561627c7850907b7685c
Vo Kar <vokar97@pm.me>
2020-01-31 20:26:56 +0300

    Add xprofile

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 ln -sfrb profile ~/.profile
-ln -sfrb remaps ~/.local/bin/remaps
+ln -sfrb xprofile ~/.xprofile
 ln -sfrb aliasrc ~/.config/aliasrc
 ln -sfrb i3config ~/.config/i3/config
 ln -sfrb init.vim ~/.config/nvim/init.vim
 ln -sfrb inputrc ~/.config/.inputrc
 ln -sfrb zshrc ~/.config/zsh/.zshrc
+ln -sfrb remaps ~/.local/bin/remaps
