a032e25ccafa3ca9eaabf38ef8cc11ecabff0641
Vo Kar <vokar97@pm.me>
2020-02-03 07:31:00 +0300

    Use i3status-rust instead of i3blocks

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ ln -sfrb profile ~/.profile
 ln -sfrb xprofile ~/.xprofile
 ln -sfrb aliasrc ~/.config/aliasrc
 ln -sfrb i3config ~/.config/i3/config
+ln -sfrb i3status ~/.config/i3/status
 ln -sfrb init.vim ~/.config/nvim/init.vim
 ln -sfrb inputrc ~/.config/.inputrc
 ln -sfrb zshrc ~/.config/zsh/.zshrc
