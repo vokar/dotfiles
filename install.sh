021a4085f37a379286750811087e7e57d1df385a
Vo Kar <vokar97@pm.me>
2020-02-04 18:06:21 +0300

    Add dunstrc

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 ln -sfrb profile ~/.profile
 ln -sfrb xprofile ~/.xprofile
+ln -sfrb inputrc ~/.config/.inputrc
 ln -sfrb aliasrc ~/.config/aliasrc
 ln -sfrb i3config ~/.config/i3/config
 ln -sfrb i3status.toml ~/.config/i3/status.toml
 ln -sfrb init.vim ~/.config/nvim/init.vim
-ln -sfrb inputrc ~/.config/.inputrc
 ln -sfrb zshrc ~/.config/zsh/.zshrc
+ln -sfrb dunstrc ~/.config/dunst/dunstrc
 ln -sfrb remaps ~/.local/bin/remaps
