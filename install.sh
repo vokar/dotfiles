c5473dcb12013a94f2786df9c96e7a6691926ed9
Vo Kar <vokar97@pm.me>
2020-02-07 18:23:16 +0300

    Add shortcutrc

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ ln -sfrb profile ~/.profile
 ln -sfrb xprofile ~/.xprofile
 ln -sfrb inputrc ~/.config/.inputrc
 ln -sfrb aliasrc ~/.config/aliasrc
+ln -sfrb shortcutrc ~/.config/shortcutrc
 ln -sfrb i3config ~/.config/i3/config
 ln -sfrb i3status.toml ~/.config/i3/status.toml
 ln -sfrb init.vim ~/.config/nvim/init.vim
